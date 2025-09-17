998cb4cdf2c4f01711d73c3f6ad853f307e19154
Marie <anikazahra33@gmail.com>
2025-09-17 16:49:22 +0500

    Create python_project_06.py

---
+++ python_project_06.py
@@ -0,0 +1,8 @@
+girl_name = " tina "
+girl_age = " 17 "
+print("she is a girl named"+ girl_name +".")
+print("she is" +  girl_age  +  "years old",".")
+girl_name = " serah "
+girl_being = " 70 "
+print("she likes name" + girl_name + ".")
+print("she didn't like being" + girl_being + ".")
