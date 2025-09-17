ce710086076feb7f39c7bd971e246c61ae6e28f3
Marie <anikazahra33@gmail.com>
2025-09-17 17:22:54 +0500

    Create Python's_project_08.py

---
+++ Python's_project_08.py
@@ -0,0 +1,4 @@
+girl_hobby = input(" Ente a girl's hobby : ")
+girl_class  = input(" Enter her class : ")
+print("she likes " + girl_hobby + ".")
+print(" she is in " +  girl_class  + " standard. ")
