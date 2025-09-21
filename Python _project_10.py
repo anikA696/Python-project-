f1b709ee35981af6a51cf1b29d716647ccc9567b
Marie <anikazahra33@gmail.com>
2025-09-21 11:11:25 +0500

    Create Python _project_10.py

---
+++ Python _project_10.py
@@ -0,0 +1,9 @@
+star = input("Enter a star:")
+verb = input("Enter the verb:")
+world = input("Enter a world:")
+diamond = input("Enter a diamond:")
+
+print(" Twinkle twinkle little " + star + "?")
+print(" How i " + verb + " what you are ?")
+print(" Up upon the " + world + " so high ")
+print (" Like a " + diamond + " in the sky ")
