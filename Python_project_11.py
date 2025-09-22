13ca5d1e97f42a53037cdd7cf14fa0166626d84f
Marie <anikazahra33@gmail.com>
2025-09-22 18:39:20 +0500

    Create Python_project_11.py

---
+++ Python_project_11.py
@@ -0,0 +1,16 @@
+food = "warm"
+
+if food == "warm":
+   print("food is warm")
+
+elif food == "cold":
+     print("food is cold")
+
+elif food == "dirty":
+     print("food is dirty")
+
+elif food == "expired":
+     print ("food is expired")
+
+else:
+    print("unknown food condition")
