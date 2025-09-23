6fbe956c69d915d1bdaee64ba3cd28ea6a95438f
Marie <anikazahra33@gmail.com>
2025-09-23 18:55:05 +0500

    Create _project_12.py

---
+++ _project_12.py
@@ -0,0 +1,7 @@
+name = input("Enter your name :")
+age =  int(input( "Enter your age :"))
+
+if age>= 18:
+   print(" Hello "  + name +  " \nyou are eligible to vote")
+else:
+    print(" Hello " + name +"\nsorry you are not eligible to vote")
